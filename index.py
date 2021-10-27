1822eecb82840b6bef213fc5359e74c7c149602a
Yoshtexnik <87989049+Yoshtexnik@users.noreply.github.com>
2021-10-27 14:45:51 +0500

    Update and rename index.php to index.py

---
+++ index.py
@@ -0,0 +1,17 @@
+from aiogram import Bot, Dispatcher, executor, types
+API_TOKEN = 'Tokeniz'
+import time
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    millisec = round(time.time() * 1000)
+    send = await message.answer("Bot tezligi: 0 ms")
+    current = round(time.time() * 1000) - millisec
+    xabar = "Bot tezligi: " + str(current) + " ms"
+    await send.edit_text(xabar)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
